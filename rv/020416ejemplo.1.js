6accf01d692a16054180156fd30c7c765499538f
jruben-fgonzalez <hunter_924@hotmail.com>
2016-03-04 14:06:49 -0600

    Update and rename ejemplo.1.js to 020416ejemplo.1.js

--- rv/020416ejemplo.1.js
+++ rv/020416ejemplo.1.js
@@ -1,5 +1,5 @@
 function Pierna(){
-  THREE.object3D.call(this);
+  THREE.Object3D.call(this);
   this.pierna=new THREE.Mesh(new THREE.BoxGeometry(1,5,1));
   this.pie=new THREE.Mesh(new THREE.BoxGeometry(2,1,1));
   this.pierna.position.y=-2.5;
