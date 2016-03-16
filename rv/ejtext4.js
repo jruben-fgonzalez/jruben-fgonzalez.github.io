375c2b6d42e65ff1fad62b3d8919bfa6d4ed590c
jruben-fgonzalez <hunter_924@hotmail.com>
2016-03-16 11:04:38 -0600

    Create ejtext4.js

---
+++ rv/ejtext4.js
@@ -0,0 +1,34 @@
+function setup(){
+        THREE.ImageUtils.crossOrigin='';
+        var textura= THREE.ImageUtils.loadTexture('http://threejs.org/examples/textures/planets/earth_atmos_2848.jpg');
+        var material= new THREE.MeshPhongMaterial({map:textura});
+        var forma=new THREE.SphereGeometry(1,10);
+        malla= new THREE.Mesh(forma,material);
+        malla.rotation.z+=0.25;
+  
+        var luzpuntual=new THREE.PointLight(0xFFFFFF);
+        luzpuntual.position.x=10;
+        luzpuntual.position.y=10;
+        luzpuntual.position.z=10;
+        
+        escena=new THREE.Scene();
+        escena.add(malla);
+        escena.add(luzpuntual);
+        
+        camara=new THREE.PerspectiveCamera();
+        camara.position.z=10;
+        renderer=new THREE.WebGLRenderer();
+        renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
+        document.body.appendChild(renderer.domElement);
+        }
+      function loop(){
+        requestAnimationFrame(loop);
+        
+        malla.rotation.y+=0.01;
+        
+        renderer.render(escena,camara);
+        }
+        
+      var escena, camara, renderer, malla;
+      setup();
+      loop();
