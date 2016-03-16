2f0eb6e16d67cddaef83258193de726855455d6b
jruben-fgonzalez <hunter_924@hotmail.com>
2016-03-16 10:57:03 -0600

    Create ejtext3.js

---
+++ rv/ejtext3.js
@@ -0,0 +1,35 @@
+function setup(){
+        THREE.ImageUtils.crossOrigin='';
+        var textura= THREE.ImageUtils.loadTexture('http://threeejs.org/examples/textures/brick_diffuse.jpg');
+        var material= new THREE.PhongLambertMaterial({map:textura});
+        var forma=new THREE.BoxGeometry(1,1,1);
+        
+        malla= new THREE.Mesh(forma,material);
+  
+        var luzpuntual=new THREE.PointLight(0xFFFFFF);
+        luzpuntual.position.x=10;
+        luzpuntual.position.y=10;
+        luzpuntual.position.z=10;
+        
+        escena=new THREE.Scene();
+        escena.add(malla);
+        escena.add(luzpuntual);
+        
+        camara=new THREE.PerspectiveCamera();
+        camara.position.z=10;
+        renderer=new THREE.WebGLRenderer();
+        renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
+        document.body.appendChild(renderer.domElement);
+        }
+      function loop(){
+        requestAnimationFrame(loop);
+        
+        malla.rotation.x+=0.01;
+        malla.rotation.y+=0.01;
+        
+        renderer.render(escena,camara);
+        }
+        
+      var escena, camara, renderer, malla;
+      setup();
+      loop();
