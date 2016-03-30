d8305f8ea3cd07d495ec47605ef7c7735fe31fb9
jruben-fgonzalez <hunter_924@hotmail.com>
2016-03-30 11:04:14 -0600

    Update colisiones2.js

--- rv/colisiones2.js
+++ rv/colisiones2.js
@@ -30,7 +30,10 @@ function loop(){
   
   if((obstaculo1.length>0 && (obstaculo1[0].distance<=0.5)) || (obstaculo2.length>0 && (obstaculo2[0].distance<=0.5))) step=-step;
 
-pelota.position.x+=step;
+  pelota.position.x+=step;
+  
+  raycaster1.set(pelota.position,new THREE.Vector3(1,0,0));
+  raycaster2.set(pelota.position,new THREE.Vector3(-1,0,0));
 
   renderer.render(escena,camara);
   requestAnimationFrame(loop);
