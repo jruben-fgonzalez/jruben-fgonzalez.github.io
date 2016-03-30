4ade6e0d82e85c4ae8616223d8357f57870e11a1
jruben-fgonzalez <hunter_924@hotmail.com>
2016-03-30 10:36:01 -0600

    Update colisiones1.js

--- rv/colisiones1.js
+++ rv/colisiones1.js
@@ -1,8 +1,8 @@
 function setup(){
-  cubo 1=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial());
-  cubo 2=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial());
-  cubo 1.position.x=0.7;
-  cubo 2.position.x=-0.7;
+  cubo1=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial());
+  cubo2=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial());
+  cubo1.position.x=0.7;
+  cubo2.position.x=-0.7;
   camara=new THREE.PerspectiveCamera();
   camara.position.z=5;
   
