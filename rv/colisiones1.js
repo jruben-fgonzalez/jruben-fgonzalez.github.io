11569d5a69ff726c07284c4f2bde01874a792ed4
jruben-fgonzalez <hunter_924@hotmail.com>
2016-03-30 10:37:02 -0600

    Update colisiones1.js

--- rv/colisiones1.js
+++ rv/colisiones1.js
@@ -13,7 +13,7 @@ function setup(){
   escena.add(cubo2);
   escena.add(camara);
   
-  renderer=new THREE.WebGlRenderer();
+  renderer=new THREE.WebGLRenderer();
   renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
   document.body.appendChild(renderer.domElement);
   step=0.01;
