567ecb862eeac2fbf525630e6e3f4c7f15a0c4ae
jruben-fgonzalez <hunter_924@hotmail.com>
2016-04-22 13:35:36 -0500

    Update cuarto.js

--- rv/cuarto.js
+++ rv/cuarto.js
@@ -91,8 +91,12 @@ spotLight.position.set(malla.position,new THREE.Vector3(1,0,0));
 
 spotLight.castShadow = true;
 
-//spotLight.shadow.mapSize.width = 1024;
-//spotLight.shadow.mapSize.height = 1024;
+spotLight.shadow.mapSize.width = 1024;
+spotLight.shadow.mapSize.height = 1024;
+
+spotLight.shadow.camera.near = 500;
+spotLight.shadow.camera.far = 4000;
+spotLight.shadow.camera.fov = 30;
 
 escena.add( spotLight );
 
