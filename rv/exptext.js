86daec0a4099cb9746bdeed49ff7ab663168f25d
jruben-fgonzalez <hunter_924@hotmail.com>
2016-04-29 13:36:08 -0500

    Update exptext.js

--- rv/exptext.js
+++ rv/exptext.js
@@ -5,7 +5,7 @@ THREE.ImageUtils.crossOrigin='';
 var pared=new THREE.BoxGeometry(500, 100, 10);
 var pared_1=new THREE.BoxGeometry(10, 100, 500);
 var ladrillo = THREE.ImageUtils.loadTexture('http://threejs.org/examples/textures/brick_diffuse.jpg');
-var dragon= THREE.ImageUtils.loadTexture('http:/');
+var dragon= THREE.ImageUtils.loadTexture('http://thumbs.dreamstime.com/z/drag%C3%B3n-blanco-y-negro-39474403.jpg');
 var material2 = new THREE.MeshPhongMaterial({map: ladrillo });
 var material = new THREE.MeshBasicMaterial({map: dragon });
 
@@ -42,7 +42,7 @@ spotLight.shadow.camera.near = 500;
 spotLight.shadow.camera.far = 4000;
 spotLight.shadow.camera.fov = 30;
 
-scene.add( spotLight );
+escena.add( spotLight );
 
 renderer=new THREE.WebGLRenderer();
 renderer.setSize(window.innerHeight*0.95,window.innerHeight*0.95);
